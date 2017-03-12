6d37244ab4f96308045c471903ee0a3e291ef08f
Michael Joseph <michaeljoseph+github@gmail.com>
2017-03-12 03:11:56 +0200

    (red) A workflow needs a repository

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -1,6 +1,9 @@
 from triage import Workflow
 
+import pytest
+
 
 def test_workflow_finds_issues():
     workflow = Workflow()
-    assert [] == workflow.find_issues()
+    with pytest.raises(Exception):
+        workflow.find_issues()
