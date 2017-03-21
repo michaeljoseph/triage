94dd608b65b206ab94c09a0dfa5963a5a62fe292
Michael Joseph <michaeljoseph+github@gmail.com>
2017-03-22 01:20:04 +0200

    Implement api methods with auth

--- tests/repository/test_github_repository.py
+++ tests/repository/test_github_repository.py
@@ -14,3 +14,42 @@ def test_gh_repo_partial_configuration():
         GithubRepository({'github_token': 'bar'})
 
     assert str(err.value) == 'Missing configuration key(s): owner,repo'
+
+
+@pytest.fixture
+def repository_config():
+    return {
+        'github_token': 'foo',
+        'owner': 'michaeljoseph',
+        'repo': 'changes'
+    }
+
+
+def test_configured_gh_repo(repository_config):
+    repo = GithubRepository(repository_config)
+
+
+@pytest.fixture
+def mock_github_requests(mocker):
+    return mocker.patch(
+        'triage.repository.github.requests',
+        autospec=True,
+    )
+
+
+def test_read_issues_requests_github_api(repository_config, mock_github_requests):
+    GithubRepository(repository_config).read_issues()
+
+    mock_github_requests.get.assert_called_once_with(
+        'https://api.github.com/repos/michaeljoseph/changes/issues',
+        headers={'Authorization': 'token foo'}
+    )
+
+
+def test_update_issue_requests_github_api(repository_config, mock_github_requests):
+    GithubRepository(repository_config).update_issue(issue_id='123', label='bug')
+
+    mock_github_requests.patch.assert_called_once_with(
+        'https://api.github.com/repos/michaeljoseph/changes/issues/123',
+        headers={'Authorization': 'token foo'}
+    )
--- triage/repository/github.py
+++ triage/repository/github.py
@@ -1,8 +1,36 @@
 from .core import Repository
 
+from uritemplate import URITemplate
+import requests
+
+
+READ_ISSUES = URITemplate('https://api.github.com/repos{/owner}{/repo}/issues')
+UPDATE_ISSUE = URITemplate('https://api.github.com/repos{/owner}{/repo}/issues{/id}')
+
+
 class GithubRepository(Repository):
     config_schema = {
         'github_token': 'string',
         'owner': 'string',
         'repo': 'string'
     }
+
+    @property
+    def headers(self):
+        return {
+            'Authorization': 'token {}'.format(self.config['github_token'])
+        }
+
+    def read_issues(self, filters=None):
+        return requests.get(
+            READ_ISSUES.expand(self.config),
+            headers=self.headers
+        )
+
+    def update_issue(self, issue_id, label):
+        config = dict(**self.config)
+        config['id'] = issue_id
+        return requests.patch(
+            UPDATE_ISSUE.expand(config),
+            headers=self.headers
+        )
